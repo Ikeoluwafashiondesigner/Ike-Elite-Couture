6187710f221f4ca59c48550bb1e7de75665d6d14
Ike stitches <danieladepoju446@gmail.com>
2026-06-16 12:26:24 +0000

    Updated website files

--- docs/script.js
+++ docs/script.js
@@ -177,4 +177,24 @@ function toggleMobileTheme(){
   
   menu.classList.toggle("show-theme");
   
-  }
+}
+const slides =
+document.querySelectorAll(".slide");
+
+let current = 0;
+
+setInterval(()=>{
+
+slides[current]
+.classList.remove("active");
+
+current++;
+
+if(current >= slides.length){
+current = 0;
+}
+
+slides[current]
+.classList.add("active");
+
+},5000);
